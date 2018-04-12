5be99f4d4e40b570d8c57580e371ed08f24d9214
Nadine <nad4ne@gmail.com>
2018-04-11 21:35:44 -0700

    react router and first component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './components/HomePage';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={HomePage} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Welcome to Eventity </h1>
+        <h3><a href='#'>Sign Up </a></h3>
+        <h3><a href='#'>Log in </a></h3>
+
+      </div>)
+  }
+}
+
+
+export default HomePage;
